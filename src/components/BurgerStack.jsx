bb9ef94332876752ebf057c9caedeec62161aa10
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-09-22 14:42:00 -0400

    MVP complete

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,20 @@
+const BurgerStack = (props) => {
+  return (
+    <ul>
+      {props.stack.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button
+            onClick={props.removeFromBurger}
+            name={ingredient.name}
+            value={ingredient.color}
+          >
+            -
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BurgerStack;
---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,20 @@
+const IngredientList = (props) => {
+  return (
+    <ul>
+      {props.ingredients.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button
+            onClick={props.addToBurger}
+            name={ingredient.name}
+            value={ingredient.color}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default IngredientList;
